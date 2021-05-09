a4c49ebd6dc185e3bd0809c96cb8bb4b8c950ee8
John Bailey <jbailey7@student05.cse.nd.edu>
2021-05-09 18:16:47 -0400

    new message and mircoplanner update

--- src/main/java/edu/nd/cse/ids/hw4/MicroPlanner.java
+++ src/main/java/edu/nd/cse/ids/hw4/MicroPlanner.java
@@ -32,16 +32,80 @@ public class MicroPlanner
 		for (int i = 0; i < documentPlan.size(); i ++) {
 			SPhraseSpec base = nlgFactory.createClause(); 
 
-			if (documentPlan.get(i) instanceof YearDiscoveredMessage) {
-				base = handleYearDiscoveredMessage((YearDiscoveredMessage)documentPlan.get(i)); 
-			} else if (documentPlan.get(i) instanceof PlanetMassMessage) {
-				base = handlePlanetMassMessage((PlanetMassMessage)documentPlan.get(i)); 
-			} else if (documentPlan.get(i) instanceof NumberPlanetsMessage) {
-				base = handleNumberPlanetsMessage((NumberPlanetsMessage)documentPlan.get(i)); 
-			} else if (documentPlan.get(i) instanceof NumberOrbitalMessage) {
-				base = handleNumberOrbitalMessage((NumberOrbitalMessage)documentPlan.get(i)); 
+			if (documentPlan.get(i) instanceof AveragePointDifferentialMessage) {
+
+				base = handleAveragePointDifferentialMessage((AveragePointDifferentialMessage)documentPlan.get(i)); 
+
+			} else if (documentPlan.get(i) instanceof BestTeamMessage) {
+
+				base = handleBestTeamMessage((BestTeamMessage)documentPlan.get(i)); 
+
+			} else if (documentPlan.get(i) instanceof DefensiveQualityMessage) {
+
+				base = handleDefensiveQualityMessage((DefensiveQualityMessage)documentPlan.get(i)); 
+
+			} else if (documentPlan.get(i) instanceof MostLossesMessage) {
+
+				base = handleMostLossesMessage((MostLossesMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof MostWinsMessage) {
+
+				base = handleMostWinsMessage((MostWinsMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof NumLossesMessage) {
+
+				base = handleNumLossesMessage((NumLossesMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof NumTiesMessage) {
+
+				base = handleNumTiesMessage((NumTiesMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof NumWinsMessage) {
+
+				base = handleNumWinsMessage((NumWinsMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof OffensiveQualityMessage) {
+
+				base = handleOffensiveQualityMessage((OffensiveQualityMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof PointDifferentialMessage) {
+
+				base = handlePointDifferentialMessage((PointDifferentialMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof PointsAgainstMessage) {
+
+				base = handlePointsAgainstMessage((PoinstAgainstMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof PointsForMessage) {
+
+				base = handlePointsForMessage((PointsForMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof ScheduleToughnessMessage) {
+
+				base = handleScheduleToughnessMessage((ScheduleToughnessMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof SuperBowlWinnerMessage) {
+
+				base = handleSuperBowlWinnerMessage((SuperBowlWinnerMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlane.get(i) instanceof SuperBowlLoserMessage) {
+				
+				base = handleSuperBowlLoserMessage((SuperBowlLostMessge)documentPlan.get(i)); 
+
+			} else if (documentPlan.get(i) instanceof TeamQualityMessage) {
+
+				base = handleTeamQualityMessage((TeamQualityMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof WinPercentageMessage) {
+
+				base = handleWinPercentageMessage((WinPercentageMessage)documentPlan.get(i)); 
+			
+			} else if (documentPlan.get(i) instanceof WorstTeamMessage) {
+
+				base = handleWorstTeamMessage((WorstTeamMessage)documentPlan.get(i)); 
+			
 			} else {
-				System.out.println("Error in MicroPlanner line 60! Wack instance"); 
+				System.out.println("Error in MicroPlanner line 104! Wack instance"); 
 
 			}
 
@@ -51,48 +115,113 @@ public class MicroPlanner
 		return specs; 
     }
     
-    public SPhraseSpec handleYearDiscoveredMessage(YearDiscoveredMessage message)
+    public SPhraseSpec handleAveragePointDifferentialMessage(AveragePointDifferentialMessage message)
     {
 		SPhraseSpec s1 = nlgFactory.createClause(); 
-		s1.setSubject("planet"); 
-		s1.setVerb("was"); 
-		String obj = "discovered in " + (int)message.getYearDiscovered(); 
-		s1.setObject(obj); 
-		s1.addFrontModifier("this"); 
 		
 		return s1; 
 	}
 
-	public SPhraseSpec handlePlanetMassMessage(PlanetMassMessage message) {
+	public SPhraseSpec handleBestTeamMessage(BestTeamMessage message) {
 		SPhraseSpec s1 = nlgFactory.createClause(); 
-		s1.setSubject("planet"); 
-		s1.setVerb("has"); 
-		String obj = "a mass of " + (int)message.getPlanetMass() + " Jupiters"; 
-		s1.setObject(obj); 
-		s1.setFrontModifier("this"); 
 
 		return s1; 
 	}
 
-	public SPhraseSpec handleNumberPlanetsMessage(NumberPlanetsMessage message) {
+	public SPhraseSpec handleDefensiveQualityMessage(DefensiveQualityMessage message) {
 		SPhraseSpec s1 = nlgFactory.createClause(); 
-		s1.setSubject("planet"); 
-		s1.setVerb("is"); 
-		String obj = "one of " + (int)message.getNumberPlanets() + " known in its system"; 
-		s1.setObject(obj); 
-		s1.setFrontModifier("this"); 
 
 		return s1; 
 	}
 
-	public SPhraseSpec handleNumberOrbitalMessage(NumberOrbitalMessage message) {
+	public SPhraseSpec handleMostLossesMessage(MostLossesMessage message) {
 		SPhraseSpec s1 = nlgFactory.createClause(); 
-		s1.setSubject("planet"); 
-		s1.setVerb("takes"); 
-		String obj = (int)message.getNumberOrbital() + " days to orbit its star"; 
-		s1.setObject(obj); 
-		s1.setFrontModifier("this"); 
 
 		return s1; 
 	}
+
+	public SPhraseSpec handleMostWinsMessage(MostWinsMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handleNumLossesMessage(NumLossesMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handleNumTiesMessages(NumTiesMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+	
+	public SPhraseSpec handleNumWinsMessage(NumWinsMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handleOffensiveQualityMessage(OffensiveQualityMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handlePointDifferentialMessage(PointDifferentialMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+	
+	public SPhraseSpec handlePointsAgainstMessage(PoinstAgainstMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handlePointsForMessage(PointsForMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handleScheduleToughnessMessage(ScheduleToughnessMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handleSuperBowlWinnerMessage(SuperBowlWinnerMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handleSuperBowlLoserMessage(SuperBowlLoserMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handleTeamQualityMessage(TeamQualityMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handleWinPercentageMessage(WinPercentageMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
+	public SPhraseSpec handleWorstTeamMessage(WorstTeamMessage message) {
+		SPhraseSpec s1 = nlgFactory.createClause(); 
+
+		return s1; 
+	}
+
 }
---
+++ src/main/java/edu/nd/cse/ids/hw4/messages/SuperBowlLoserMessage.java
@@ -0,0 +1,27 @@
+package edu.nd.cse.ids.hw4.messages;
+
+import edu.nd.cse.ids.hw4.*;
+
+public class SuperBowlLoserMessage extends Message
+{
+    private String superBowlLoser;
+    
+    public SuperBowlLoserMessage()
+    {
+    }
+    
+    public void generate(ExamEntry entry)
+    {
+		this.superBowlLoser = entry.getSuperBowlLoser(); 
+    }
+    
+    public void setSuperBowlLoser(String sl)
+    {
+		this.superBowlLoser = sl; 
+    }
+    
+    public String getSuperBowlLoser()
+    {
+		return this.superBowlLoser; 
+    }
+}
